92ae7a850f7323c6ab61558a3d9a0547b7bc895d
Tristan Blackwell <tristanblackwell1@gmail.com>
2020-12-20 10:44:50 +0000

    Clean up flow and project dependencies

--- api/controllers/symbolController.js
+++ api/controllers/symbolController.js
@@ -1,9 +1,7 @@
 var Symbol = require("../models/symbol");
-const fetch = require('node-fetch');
 var async = require("async");
 
-const { body, validationResult } = require("express-validator");
-
+// Get ticker count, all tickers and send to client
 exports.symbol_list = function(socket) {
     
     async.parallel([
@@ -14,36 +12,56 @@ exports.symbol_list = function(socket) {
             Symbol.find({}, callback);
         }
     ], function(err, result) {
-        if (err) { console.log(err); }
-        socket.emit("symbol_list", result);
+        if (err) { 
+            console.log(err); 
+            socket.emit("symbol_list", {data: err, msg: "error"})
+        } else {
+            console.log("tickers sent")
+            socket.emit("symbol_list", {data: result, msg: "success"});
+        }
     })
 }
 
+// add new ticker to DB
 exports.symbol_create = function(socket, ticker) {
 
-    var symbol = new Symbol(
-        { name: ticker }
-    );
+    if (ticker.length < 2 || ticker.length > 5) {
+        console.log("validation error")
+        socket.emit("created", {data: "validation error", msg: "error"})
+    } else {
+        var symbol = new Symbol(
+            { name: ticker }
+        );
 
-    Symbol.findOne({ "name": ticker })
+        Symbol.findOne({ "name": ticker })
         .exec( function(err, found_symbol ) {
-            if (err) { res.json({created: false, msg: "Error in creation."}) }
+            if (err) { 
+                console.log("error in request")
+                socket.emit("created", {data: err, msg: "error"}) 
+            }
             if (found_symbol) {
-                socket.emit("created", { msg: "Symbol exists"});
+                console.log("symbol already exists")
+                socket.emit("created", { data: err, msg: "error"});
             } else {
                 symbol.save(function(err) {
-                    if (err) { res.json({created: false, msg: "Error in creation"}); }
-                    socket.emit("created", { msg: "Symbol saved" });
+                    if (err) { 
+                        console.log("error in creation")
+                        socket.emit("created", { data: err, msg: "error" }); 
+                    }
+                    console.log("symbol created")
+                    socket.emit("created", { data: null, msg: "success" });
                 })
             }
         })
+    }
 }
 
+// Remove ticker from DB
 exports.symbol_delete = function(socket, name) {
     
     Symbol.findOneAndDelete({"name": name}, function(err) {
-        if (err) { console.log(err); }
-        socket.emit("deleted", { msg: "Symbol deleted" });
+        if (err) { console.log("error in deletion"); }
+        socket.emit("deleted", { data: null, msg: "success" });
 
     })
 }
